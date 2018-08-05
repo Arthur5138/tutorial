5e24cc4eb2530fac3c6fdd533386840c3fbc5d35
arthur5138 <arthur5138@hotmail.com>
2018-08-05 11:03:55 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    #@user = User.new(params[:user]) userオブジェクトにuserのパラムス全部を渡すのは危険,admin属性を入れられる危険性
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation) #渡される属性を制限する。
   end
 end
